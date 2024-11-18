565eda02e187e75eb05c02e2a13090d9692a09bb
gabo87654321 <gabyaguirre84@gmail.com>
2024-11-17 23:16:49 -0300

    se elimina proyectogrupobackend

--- ProjectosBackend/Tp1/apps/servicio/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from apps.servicio.models import Servicio
-# Register your models here.
-@admin.register(Servicio)
-class ServicioAdmin(admin.ModelAdmin):
-    list_display=["nombre", "descripcion"]
--- ProjectosBackend/Tp1/apps/turno/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from apps.turno.models import Turno
-# Register your models here.
-@admin.register(Turno)
-class TurnoAdmin(admin.ModelAdmin):
-    list_display=["usuario", "profesional", "servicio", "fecha_hora"]
